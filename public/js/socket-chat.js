56ab686167ca694a5560188fb7b0d84536e02021
Moises Barillas <moisesrocha717@gmail.com>
2020-12-08 14:43:55 -0600

    chat finalizado v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -2,9 +2,9 @@ var socket = io();
 
 var params = new URLSearchParams(window.location.search);
 
-if (!params.has("nombre") || !params.get("sala")) {
+if (!params.has("nombre") || !params.has("sala")) {
 	window.location = "index.html";
-	throw new Error("El nombre y sala son requeridos");
+	throw new Error("El nombre y sala son necesarios");
 }
 
 var usuario = {
@@ -16,7 +16,8 @@ socket.on("connect", function () {
 	console.log("Conectado al servidor");
 
 	socket.emit("entrarChat", usuario, function (resp) {
-		console.log("Usuarios connectados", resp);
+		// console.log("Usuarios conectados", resp);
+		renderizarUsuarios(resp);
 	});
 });
 
@@ -26,30 +27,26 @@ socket.on("disconnect", function () {
 });
 
 // Enviar información
-/*socket.emit(
-	"crearMensaje",
-	{
-		usuario: "Fernando",
-		mensaje: "Hola Mundo",
-	},
-	function (resp) {
-		console.log("respuesta server: ", resp);
-	}
-);*/
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on("crearMensaje", function (mensaje) {
-	console.log("Servidor:", mensaje);
+	renderizarMensajes(mensaje, false);
+	scrollBottom();
 });
 
-//Escuchar cambios de usuarios
-
-socket.on("listaPersonas", function (personas) {
-	console.log(personas);
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
+socket.on("listaPersona", function (personas) {
+	renderizarUsuarios(personas);
 });
 
-// Mensaje privados
-
+// Mensajes privados
 socket.on("mensajePrivado", function (mensaje) {
-	console.log("mensaje privado", mensaje);
+	console.log("Mensaje Privado:", mensaje);
 });
